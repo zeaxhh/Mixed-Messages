03a811f15d047dc8dc1b51371e4e6b6c7f14e612
Zachary Anderson <Zachary.sheatyler@gmail.com>
2021-03-01 20:32:56 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mixed Messages (CodeAcademy Project)
+
+>Project Objectives:
+
+* Build a message generator program using JavaScript
+* Use Git version control
+* Use command line
+* Develop locally on your computer
---
+++ main.js
@@ -0,0 +1,10 @@
+/* For this project, you will build a message generator program. Every time a user runs a program, they should get a new, randomized output. 
+You’re welcome to take the project in a couple of different forms, like an astrology generator, inspirational message, or nonsensical jokes. 
+To make your program truly random, the message that it outputs should be made up of at least three different pieces of data. Take what you 
+know of JavaScript syntax so far to build the program and customize it to your liking. */
+
+const foodGroups ={
+    Proteins : ['steak', 'chicken', 'tofu', 'fish'],
+    Carbs: ['Jasmine Rice', 'Lentils','Black beans', 'Sweet Potatoes'],
+    Veggies: ['Brocolli', 'Mixed Veggies', 'Asparagus', 'Green Beans'],
+}
