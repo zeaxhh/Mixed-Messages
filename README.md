a988a089d577dcc4f269baafa8047c51127a1ac6
Zachary Anderson <Zachary.sheatyler@gmail.com>
2021-03-01 21:22:48 -0500

    commit, first working version

--- README.md
+++ README.md
@@ -1,8 +1,48 @@
 # Mixed Messages (CodeAcademy Project)
 
->Project Objectives:
+## Introduction
+
+>This Javascript program will generate a random meal, consisting of 3 seperate items. It will randomly choose 1 of 4 proteins, carbs and vegetables, and return the meal in a statement to you. It has an optional name parameter, to give a more personalized touch if you so choose.
+
+## Project Objectives
 
 * Build a message generator program using JavaScript
 * Use Git version control
 * Use command line
 * Develop locally on your computer
+
+## Technologies
+
+* GIT and GitHub
+
+* Javascript
+
+* HTML
+
+## Instructions for Use
+
+There are two methods you can use to run this program. The first is Node, the second is through your browser. We will go over Node first.
+
+### **Node**
+
+1. Download Repository to your computer
+2. Navigate to Repository using git bash or command line
+3. Enter `node main.js` into GIT, as many times as you like to generate different meals.
+
+Name parameter cannot be provided this way
+
+### **Browser**
+
+1. Download repository to your computer
+
+2. Navigate to repository using file explorer
+
+3. Open `test.html`
+
+4. Press ctrl + f12 to see dev panel and console
+
+5. Refresh as many times as you like for multiple meals
+
+*Optional*
+
+6. Open `main.js` in an editor, and change the argument provided when calling the function, `healthyMeal()`, to add your name(:
--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-/* For this project, you will build a message generator program. Every time a user runs a program, they should get a new, randomized output. 
-You’re welcome to take the project in a couple of different forms, like an astrology generator, inspirational message, or nonsensical jokes. 
-To make your program truly random, the message that it outputs should be made up of at least three different pieces of data. Take what you 
-know of JavaScript syntax so far to build the program and customize it to your liking. */
+//CodeAcademy Portfolio Project
 
+//stores nested arrays neatly inside object properties
 const foodGroups = {
-  Proteins: ["steak", "chicken", "tofu", "fish"],
+  Proteins: ["Steak", "Chicken", "Tofu", "Fish"],
   Carbs: ["Jasmine Rice", "Lentils", "Black beans", "Sweet Potatoes"],
   Veggies: ["Brocolli", "Mixed Veggies", "Asparagus", "Green Beans"],
 };
 
+//picks a random number between 1-4 to use to index arrays and choose random element
 let index = Math.floor(Math.random() * 4);
 
-const healthyMeal = (arg) => {
-    return `Hello, ${arg}, your meal will be ${foodGroups.Proteins[index]}, with ${foodGroups.Carbs[index]} and ${foodGroups.Veggies[index]}`;
+//uses template literal to combine 3 pieces of message into one, with optional 'name' feature.
+let healthyMeal = (arg) => {
+    let name = arg;
+    if(arg === undefined){
+        name = 'friend';
+    };
+    return `Hello, ${name}, your meal will be ${foodGroups.Proteins[index]}, with ${foodGroups.Carbs[index]} and ${foodGroups.Veggies[index]}`;
 };
 
-console.log(healthyMeal(Zach));
+console.log(healthyMeal());
