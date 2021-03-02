9bba5357bbc0240067107e4a0c75ef9ee159249b
Zachary Anderson <Zachary.sheatyler@gmail.com>
2021-03-01 21:44:28 -0500

    commented main.js

--- main.js
+++ main.js
@@ -7,17 +7,21 @@ const foodGroups = {
   Veggies: ["Brocolli", "Mixed Veggies", "Asparagus", "Green Beans"],
 };
 
-//picks a random number between 1-4 to use to index arrays and choose random element
-
 //uses template literal to combine 3 pieces of message into one, with optional 'name' feature.
 let healthyMeal = (arg) => {
+
+    //picks a random number between 1-4 to use to index arrays and choose random element
     let index1 = Math.floor(Math.random() * 4);
     let index2 = Math.floor(Math.random() * 4)
     let index3 = Math.floor(Math.random() * 4)
+
+    // replaces name with 'friend' if left blank
     let name = arg;
     if(arg === undefined){
         name = 'friend';
     };
+
+    //returns meal
     return `Hello, ${name}, your meal will be ${foodGroups.Proteins[index1]}, with ${foodGroups.Carbs[index2]} and ${foodGroups.Veggies[index3]}`;
 };
 
