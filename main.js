aba33c3f4a39d2a093f598815e0a169547f6b85a
Zachary Anderson <Zachary.sheatyler@gmail.com>
2021-03-01 20:41:39 -0500

    generator first revision

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ You’re welcome to take the project in a couple of different forms, like an ast
 To make your program truly random, the message that it outputs should be made up of at least three different pieces of data. Take what you 
 know of JavaScript syntax so far to build the program and customize it to your liking. */
 
-const foodGroups ={
-    Proteins : ['steak', 'chicken', 'tofu', 'fish'],
-    Carbs: ['Jasmine Rice', 'Lentils','Black beans', 'Sweet Potatoes'],
-    Veggies: ['Brocolli', 'Mixed Veggies', 'Asparagus', 'Green Beans'],
-}
+const foodGroups = {
+  Proteins: ["steak", "chicken", "tofu", "fish"],
+  Carbs: ["Jasmine Rice", "Lentils", "Black beans", "Sweet Potatoes"],
+  Veggies: ["Brocolli", "Mixed Veggies", "Asparagus", "Green Beans"],
+};
+
+let index = Math.floor(Math.random() * 4);
+
+const healthyMeal = (arg) => {
+    return `Hello, ${arg}, your meal will be ${foodGroups.Proteins[index]}, with ${foodGroups.Carbs[index]} and ${foodGroups.Veggies[index]}`;
+};
+
+console.log(healthyMeal(Zach));
